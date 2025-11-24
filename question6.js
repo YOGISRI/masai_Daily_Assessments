9d67275349d9a81d7506f617e76e832192ec87f6
yogisri <medisettyyogisri@gmail.com>
2025-11-24 22:51:26 +0530

    question6

---
+++ question6.js
@@ -0,0 +1,7 @@
+// a. Write an arrow function 'isEven' that takes a number as input and returns true if the number is even, and false otherwise.
+const isEven = n => n % 2 === 0;
+// b. Terinary Operator
+const result = marks >= 35 ? "Pass" : "Fail";
+// c. Arrow function Terinary Operator
+const greet = name => `Hello, ${name ? name : "Guest"}`;
+
