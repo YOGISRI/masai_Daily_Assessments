8699fde178d0fca702965e3ef2be66effcb32774
yogisri <medisettyyogisri@gmail.com>
2025-11-30 17:32:22 +0530

    Question 11

---
+++ question11/question11.js
@@ -0,0 +1,9 @@
+
+document.getElementById("main-title").textContent = "Welcome to the DOM World!";
+let paragraphs = document.getElementsByTagName("p");
+for (let p of paragraphs) {
+    p.style.color = "blue";
+}
+
+let containerDiv = document.querySelector(".container");
+containerDiv.style.backgroundColor = "yellow";
