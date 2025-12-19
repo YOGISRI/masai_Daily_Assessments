9c7d0230348dcea7e4d5a39bf7c19f53aef4012c
yogisri <medisettyyogisri@gmail.com>
2025-12-19 20:27:12 +0530

    TodoList

---
+++ todo-list/src/TodoCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TodoCard = ({ userId, title, completed }) => {
+    return (
+        <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+            <p><strong>User ID:</strong> {userId}</p>
+            <p><strong>Title:</strong> {title}</p>
+            <p>
+                <strong>Status:</strong>{" "}
+                {completed ? "Completed" : "Not Completed"}
+            </p>
+        </div>
+    );
+};
+
+export default TodoCard;
